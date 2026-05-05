48b85648349c1c99c0d8f799bb4c0d0de22083fc
Neo-Serwis <biuro@neo-serwis.net>
2026-05-05 08:35:39 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — auto-inwalidacja cache przez timestamp
-const CACHE = 'trening-v3-20260504b';
+const CACHE = 'trening-v3-20260504c';
 
 self.addEventListener('install', e => {
   e.waitUntil(
